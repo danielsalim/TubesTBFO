f216775f4d6dee013c49534cf8972b9fc3ac16b0
arikrayi <83354451+arikrayi@users.noreply.github.com>
2021-11-24 19:37:19 +0700

    kesandung

--- CFGtoCNF.py
+++ CFGtoCNF.py
@@ -1,11 +1,10 @@
 import itertools
-
 left, right = 0, 1
-K, V, Productions = [],[],[]
-variablesJar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "W", "X", "Y", "Z",
-"A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1", "I1", "J1", "K1", "L1", "M1", "N1", "O1", "P1", "Q1", "R1", "S1", "T1", "U1", "W1", "X1", "Y1", "Z1",
-"A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2", "I2", "J2", "K2", "L2", "M2", "N2", "O2", "P2", "Q2", "R2", "S2", "T2", "U2", "W2", "X2", "Y2", "Z2",
-"A3", "B3", "C3", "D3", "E3", "F3", "G3", "H3", "I3", "J3", "K3", "L3", "M3", "N3", "O3", "P3", "Q3", "R3", "S3", "T3", "U3", "W3", "X3", "Y3", "Z3"]
+variablesJar = ["A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1", "I1", "J1", "K1", "L1", "M1", "N1", "O1", "P1", "Q1", "R1", "S1", "T1", "U1", "V1", "W1", "X1", "Y1", "Z1",
+"A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2", "I2", "J2", "K2", "L2", "M2", "N2", "O2", "P2", "Q2", "R2", "S2", "T2", "U2", "V2", "W2", "X2", "Y2", "Z2",
+"A3", "B3", "C3", "D3", "E3", "F3", "G3", "H3", "I3", "J3", "K3", "L3", "M3", "N3", "O3", "P3", "Q3", "R3", "S3", "T3", "U3", "V3", "W3", "X3", "Y3", "Z3",
+"A4", "B4", "C4", "D4", "E4", "F4", "G4", "H4", "I4", "J4", "K4", "L4", "M4", "N4", "O4", "P4", "Q4", "R4", "S4", "T4", "U4", "V4", "W4", "X4", "Y4", "Z4",
+"A5", "B5", "C5", "D5", "E5", "F5", "G5", "H5", "I5", "J5", "K5", "L5", "M5", "N5", "O5", "P5", "Q5", "R5", "S5", "T5", "U5", "V5", "W5", "X5", "Y5", "Z5"]
 
 
 def cleanProduction(expression):
@@ -47,13 +46,26 @@ def rewrite(target, production):
 	return result
 
 
+def convertForm(rules):
+	dictionary = {}
+	for rule in rules:
+		if rule[left] in dictionary:
+			dictionary[rule[left]] += ' | '+' '.join(rule[right])
+		else:
+			dictionary[rule[left]] = ' '.join(rule[right])
+	result = ""
+	for key in dictionary:
+		result += key+" -> "+dictionary[key]+"\n"
+	return result
+
+
 def isUnitary(rule, variables):
 	if rule[left] in variables and rule[right][0] in variables and len(rule[right]) == 1:
 		return True
 	return False
 
 
-def isSimple(rule):
+def isSimple(rule, K, V):
 	if rule[left] in V and rule[right][0] in K and len(rule[right]) == 1:
 		return True
 	return False
@@ -64,18 +76,18 @@ def newS(productions, variables): #Add S0->S rule
 	return [('S0', [variables[0]])] + productions
 
 
-def eliminateTerminalWithTerminal(productions, variables): #Remove rules like A->Bc and replaced  by A->BZ and Z->c
+def eliminateTerminalWithTerminal(productions, variables, terms): #Remove rules like A->Bc and replaced  by A->BZ and Z->c
 	newProductions = []
-	dictionary = setupDict(productions, variables, terms=K)
+	dictionary = setupDict(productions, variables, terms)
 	for production in productions:
-		if isSimple(production):
+		if isSimple(production, terms, variables):
 			newProductions.append(production)
 		else:
-			for term in K:
+			for term in terms:
 				for index, value in enumerate(production[right]):
 					if term == value and not term in dictionary:
 						dictionary[term] = variablesJar.pop()
-						V.append(dictionary[term])
+						variables.append(dictionary[term])
 						newProductions.append( (dictionary[term], [term]) )
 						production[right][index] = dictionary[term]
 					elif term == value:
@@ -103,23 +115,6 @@ def eliminateNonUnitry(productions, variables): #Eliminate non unitry rules
 	return result
 
 
-def eleminateNonTerminal(productions): #Delete non terminal rules
-	newSet = []
-	outlaws, erased = [],[]
-	for production in productions:
-		if 'e' in production[right] and len(production[right]) == 1:
-			outlaws.append(production[left])
-		else:
-			erased.append(production)
-	production = erased
-	for outlaw in outlaws:
-		for production in productions + [e for e in newSet if e not in productions]:
-			if outlaw in production[right]:
-				newSet = newSet + [e for e in  rewrite(outlaw, production) if e not in newSet]
-	return newSet + ([productions[i] for i in range(len(productions)) 
-							if productions[i] not in newSet])
-
-
 def unit_routine(rules, variables):
 	unitaries, result = [], []
 	for aRule in rules:
@@ -144,17 +139,18 @@ def unit(productions, variables):
 		i+=1
 	return result
 
-global CNF
-CNF = {}
-
-def convertToMap (Production):
-	for i in range (len(Production)):
-		s = ''
-		for j in range (len(Production[i][1])):
-			s = s + Production[i][1][j]
-		CNF.update({s : Production[i][0]})
+def convertToMap (Productions):
+	dictionary = {}
+	for production in Productions :
+		if(production[left] in dictionary.keys()):
+			dictionary[production[left]].append(production[right])
+		else :
+			dictionary[production[left]] = []
+			dictionary[production[left]].append(production[right])
+	return dictionary
 
 def convert ():
+	K, V, Productions = [],[],[]
 	try:
 		file = open('grammar.txt').read()
 		K, V, Productions = splitFile(file)
@@ -162,13 +158,18 @@ def convert ():
 			if nonTerminal in variablesJar:
 				variablesJar.remove(nonTerminal)
 		Productions = newS(Productions, variables=V)
-		Productions = eliminateTerminalWithTerminal(Productions, variables=V)
+		print(Productions)
+		print("batas suci 1")
+		Productions = eliminateTerminalWithTerminal(Productions, variables=V, terms=K)
+		print(Productions)
+		print("batas suci 2")
 		Productions = eliminateNonUnitry(Productions, variables=V)
-		Productions = eleminateNonTerminal(Productions)
+		print(Productions)
+		print("batas suci 3")
 		Productions = unit(Productions, variables=V)
-		convertToMap(Productions)
-		return CNF
+		print(Productions)
+		print("batas suci 5")
+		Productions = convertToMap(Productions)
+		return Productions
 	except:
 		print("CFG grammar 'grammar.txt' not found")
-
-Line = convert()
